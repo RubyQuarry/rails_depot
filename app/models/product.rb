54db545f0ddfeb6c1c3787588dd865169710e4c2
A.J. Norton <ajn123@vt.edu>
2014-06-08 22:59:05 -0700

    Tried formatting main page, looks ugly.
    Also tried switching between rails 4.1.1 and 4.0.0, faild miserably.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,14 @@ class Product < ActiveRecord::Base
   validates :title,:description,:image_url,presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 
+
+
   validates :title, uniqueness: true
 
   validates :image_url, allow_blank: true, format:{
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'mush be a URL for gif, jpg or png image.'
   }
+
+  validates :title, length: {minimum: 10, maximum: 32, message: "message must be between 10 and 32 characters"}
 end
