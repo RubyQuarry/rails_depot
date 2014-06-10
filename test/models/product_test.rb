3167bc9cdc53ad3285aa16df64b6c3cdd83d0653
A.J. Norton <ajn123@vt.edu>
2014-06-09 20:48:17 -0700

    Made tests more easier and complete.
    Added new_product method to aid tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,7 +2,16 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 
+
   fixtures :products
+
+  def new_product(args)
+    Product.new(title: args.fetch(:title, "Harry Potter"),
+                description: args.fetch(:description,'This a fine book'),
+                price: args.fetch(:price, 1.00),
+                image_url: args.fetch(:image_url,"muffins.png"))
+  end
+
 
   test "product attributes must not be empty" do
     product = Product.new
@@ -14,43 +23,41 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title: "My book is long",
-                          description: "yyy",
-                          image_url: "zzz.jpg")
+    product = new_product({})
     product.price = -1
     assert product.invalid?
     product.price = 1
     assert product.valid?
   end
 
-  def new_product(image_url)
-    Product.new(title:'my title is long',
-                description: 'definitions',
-                price: 1.00,
-                image_url: image_url)
-  end
 
   test "image url" do
     ok = %w{ fred.gif fred2.jpg FRED3.JPG FRED4.Jpg http://a.b.c/fred.gif }
     bad = %w{ fred.doc fred.gif.more}
 
     ok.each do |name|
-      assert new_product(name).valid? , "#{name} is invalid"
+      assert new_product({image_url: name}).valid? , "#{name} is invalid"
     end
 
     bad.each do |name|
-      assert new_product(name).invalid?,"#{name} is valid"
+      assert new_product({image_url: name}).invalid?,"#{name} is valid"
     end
   end
 
 
   test "product is not valid without a unique title" do
-    product = Product.new(title: products(:ruby).title,
-                          description: "yyy",
-                          price: 1,
-                          image_url: "fred.gif")
+
+    product = new_product({title: products(:ruby).title})
     assert product.invalid?
     assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
+  test "title must be of length between 10 and 32 inclusive" do
+    product = new_product({title: 'hello'})
+    assert product.invalid?
+
+    product.title = 'Hello, World'
+    assert product.valid?
   end
 
 
