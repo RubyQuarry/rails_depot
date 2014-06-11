37189e2646b232ac543df723e9a118cc5749ef03
A.J. Norton <ajn123@vt.edu>
2014-06-10 21:00:27 -0700

    fixed spelling mistake in current_cart.rb causing failures

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,15 @@
 class Product < ActiveRecord::Base
 
   has_many :line_items
-
   before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title,:description,:image_url,presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-
-
-
   validates :title, uniqueness: true
-
   validates :image_url, allow_blank: true, format:{
       with: %r{\.(gif|jpg|png)\Z}i,
       message: 'mush be a URL for gif, jpg or png image.'
   }
-
   validates :title, length: {minimum: 10, maximum: 32, message: 'message must be between 10 and 32 characters'}
 
   def self.latest
