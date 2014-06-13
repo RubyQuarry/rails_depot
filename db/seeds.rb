9fe5281fd0ccb8b04062e7556bde74ad8670c5b1
A.J. Norton <ajn123@vt.edu>
2014-06-13 00:31:45 -0700

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Product.create!(title: "programming ruby",
-description: %{<p> Ruby is the fastest growing and most exiting
-dynamic language out there.  If you need to get working programs delivered fast.
-  </p>},
-image_url: 'door.jpg',
-price: 49.95)
+Product.create!(title: "programming rubye",
+                description: %{<p> Ruby is the fastest growing and most exiting
+                dynamic language out there.  If you need to get working programs delivered fast.
+                  </p>},
+                image_url: 'door.jpg',
+                price: 49.95)
 
 
 
-Product.create!(title: "Muffins",
+Product.create!(title: "Muffins are hereee",
                 description: %{<p> This is muffin  </p>},
                 image_url: 'muffin.jpg',
                 price: 49.95)
+
+
+Product.create!(title: "YOLO for allee",
+                description: %{<p> We love Cats.</p>},
+                image_url: 'door.jpg',
+                price: 19.99)
